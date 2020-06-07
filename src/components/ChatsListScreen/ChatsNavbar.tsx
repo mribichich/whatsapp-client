03717cc67f4c4401539ef1607cac1708b67c18fc
Matias Ribichich <guli4073@gmail.com>
2020-06-07 17:40:16 -0300

    Added styles to ChatsNavbar

--- src/components/ChatsListScreen/ChatsNavbar.tsx
+++ src/components/ChatsListScreen/ChatsNavbar.tsx
@@ -1,5 +1,14 @@
+import Toolbar from '@material-ui/core/Toolbar';
 import React from 'react';
+import styled from 'styled-components';
 
-const ChatsNavbar: React.FC = () => <div>Whatsapp Clone</div>;
+const Container = styled(Toolbar)`
+  background-color: var(--primary-bg);
+  color: var(--primary-text);
+  font-size: 20px;
+  line-height: 40px;
+`;
+
+const ChatsNavbar: React.FC = () => <Container>Whatsapp</Container>;
 
 export default ChatsNavbar;
