1bab368593ee4c5b1fcb33fb8bec70d91bcbe941
Matias Ribichich <guli4073@gmail.com>
2020-06-07 17:40:02 -0300

    Added styles to ChatsListScreen

--- src/components/ChatsListScreen/index.tsx
+++ src/components/ChatsListScreen/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 import ChatsList from './ChatsList';
 import ChatsNavbar from './ChatsNavbar';
 
+const Container = styled.div`
+  height: 100vh;
+`;
+
 const ChatsListScreen: React.FC = () => (
   <div>
-    <ChatsNavbar />
-    <ChatsList />
+    <Container>
+      <ChatsNavbar />
+      <ChatsList />
+    </Container>
   </div>
 );
 
